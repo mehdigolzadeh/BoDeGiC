e216e243e55725ca0499d941a9b76198678890ad
Mehdi Golzadeh <53336456+mehdigolzadeh@users.noreply.github.com>
2021-11-24 12:31:26 +0100

    updated dependencies to be able to work with python 3.9 as well

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ __classifiers__=[
 __requirement__ = [
         'python-dateutil >= 2.7.5',
         'pandas >= 0.23.4',
-        'scikit-learn == 0.22',
+        'scikit-learn >= 0.22',
         'argparse >= 1.1',
         'GitPython >= 3.1.8',
         'tqdm >= 4.41.1',
